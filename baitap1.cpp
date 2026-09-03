cc68103a4d6decf4ef733cf4b2bbfe5a4ad84138
Vinh08-coder <buithevinh2810@gmail.com>
2026-09-03 20:48:16 +0700

    Add files via upload

---
+++ baitap1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+int canh;
+cout <<"canh";
+cin>>canh;
+int chuvi=canh*canh;
+cout << "chuvihinhvuong" << chuvi<<endl;
+return 0;
+}
+
+
+
+
+
+
+
+
+
---
+++ baitap2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+int giatien, sosanpham;
+cout<<"gia tien";
+cin>>giatien;
+cout<<"sosanpham";
+cin>>sosanpham;
+int doanhthu=giatien*sosanpham;
+cout<<"tong so tien ban duoc"<<doanhthu<<endl;
+return 0;
+}
+
+
+
+
+
